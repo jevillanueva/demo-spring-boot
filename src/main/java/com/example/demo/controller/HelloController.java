ecdd01ab1a796ae65cb9bbd23d93071c5124b5b8
Jonathan E. Villanueva Villanueva <frik_ej2v@hotmail.com>
2021-06-27 12:09:37 -0400

    jsp and rest

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -1,12 +1,18 @@
 package com.example.demo.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
-@RestController
+@Controller
 public class HelloController {
-    @RequestMapping("/")
-    public String index(){
-        return "Greetings from Spring Boot";
+
+    @RequestMapping("/test")
+    public String viewHello(Model model) {
+        model.addAttribute("data", "hellow");
+        return "indexs";
     }
 }
---
+++ src/main/webapp/WEB-INF/jsp/indexs.jsp
@@ -0,0 +1,12 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<html>
+    <head>
+        <title>View Books</title>
+        <link href="<c:url value="/css/common.css"/>" rel="stylesheet" type="text/css">
+    </head>
+    <body>
+        <h2>sad</h2>
+       <h1>${data}</h1>
+    </body>
+</html>
